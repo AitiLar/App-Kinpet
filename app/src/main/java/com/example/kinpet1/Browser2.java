c881ace9884e58a3724186cf86ff4d1682b7d501
= <=>
2021-01-11 15:23:50 +0300

    commit

---
+++ app/src/main/java/com/example/kinpet1/Browser2.java
@@ -0,0 +1,14 @@
+package com.example.kinpet1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Browser2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser2);
+    }
+}
